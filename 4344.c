e75c953b85db650a4f0bd8827bd321682b230653
ParkHyejin <jin050798@naver.com>
2022-08-29 15:26:57 +0900

    4344

--- 4344.c
+++ 4344.c
@@ -1,23 +1,32 @@
 #include<stdio.h>
+#include<stdlib.h>
+
 int main(){
 	int test_num;
 	int student_num;
 	int *score;
-	score = (int*)malloc(sizeof(student_num));
 	printf("테스트 케이스의 개수를 입력하세요 : ");
 	scanf("%d",&test_num);
 	int k = 0;
-	int i = 0;
-	int j = 0;
-	int l = 0;
-	double t = 0;
-	int total = 0;
-	int avg = 0;
-	int count1=0,count2=0;
+	int i;
+	int j;
+	int l;
+	double t;
+	int total;
+	double avg;
+	int count1;
 	while(k<test_num){
+		count1 = 0;
+		i=0;
+		t=0;
+		j=0;
+		l=0;
+		total = 0;
+		avg = 0;
 		scanf("%d",&student_num);
+		score = (int*)malloc(sizeof(int)*student_num);
 		while(i<student_num){
-			scanf("%d",score);
+			scanf("%d",&score[i]);
 			i++;
 		}
 		while(j<student_num){
@@ -26,15 +35,14 @@ int main(){
 		}
 		avg = total/student_num;
 		while(l<student_num){
-			if(score[l]<avg){
+			if(score[l]>avg){
 				count1++;
-			}else{
-				count2++;
 			}
 			l++;
 		}
-		t = count2/student_num;
-		printf("%f\n",&t);
+		t = (double)count1/(double)student_num;
+		printf("%.3f\n",t*100);
+		free(score);
 		k++;
 	}
 }
