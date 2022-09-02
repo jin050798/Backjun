dedcfa36a1d01658b5edacc4868169e98ce1767b
ParkHyejin <jin050798@naver.com>
2022-09-02 10:39:46 +0900

    space search

--- 문자열/1152.c
+++ 문자열/1152.c
@@ -4,15 +4,31 @@ int main() {
     char arr[1000000];
     int count=0;
     int len;
-    scanf("%[^\n]s", arr);
+    scanf("%[^\n]s", arr);//공백을 포함한 문자열 입력받기
     len = strlen(arr);
     for (int i = 0; i < len; i++) {
         if (arr[i] == ' ') {
             count++;
         }
     }
-    if(arr[0]==' '||arr[len]==' '){
-	    count = count-1;
+    int j = 0;
+    int space_num = 0;
+    if(arr[0]==' '){
+	    while(arr[j] == ' '){
+		    space_num++;
+		    j++;
+	    }
+	    count = count - space_num;
     }
+    int space_num2 = 0;
+    int k = strlen(arr);
+    if(arr[len-1]==' '){
+	    while(arr[k-1] == ' '){
+		    space_num2++;
+		    k--;
+	    }
+	    count = count - space_num2;
+    }
+
     printf("%d", count + 1);
 }
